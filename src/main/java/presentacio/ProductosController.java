cc179deb2eb46fce92b760dcf7cb92ab7e70b427
trujillo.agut.carles <trujillo.agut.carles@alumnat.copernic.cat>
2022-12-19 14:57:53 +0100

    Insert de productos

--- src/main/java/presentacio/ProductosController.java
+++ src/main/java/presentacio/ProductosController.java
@@ -13,48 +13,47 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import model.Producto;
 
 /**
  * @author D
  * @version 1.0
- * 
+ *
  * Controlador Pantalla Productos
  */
-
 public class ProductosController implements Initializable {
 
-    private LogicaProducto cl;
-    
     private int idProductoSeleccionado;
 
     // Botones
-    @FXML
-    private Button btn_atras;
     @FXML
     private Button btn_edit;
     @FXML
     private Button btn_add;
     @FXML
     private Button btn_delete;
-    
+
+    // TextFields
+    @FXML
+    private TextField txtNombre;
+    @FXML
+    private TextField txtDesc;
+    @FXML
+    private TextField txtStock;
+    @FXML
+    private TextField txtPrecio;
+
     @FXML
     private TableView<Producto> tablaProductos;
-    
+
     // Columnas
-    @FXML 
-    private TableColumn idCol, nameCol, descCol, stockCol, precioCol;
-    
-    ObservableList<Producto> llistaObservableProductos = FXCollections.observableArrayList();
-    
-    /**
-     * Initializes the controller class.
-     */
-    
     @FXML
-    private void onClickAtras(ActionEvent event) {
-    }
+    private TableColumn idCol, nombreCol, descCol, stockCol, precioCol;
+
+    ObservableList<Producto> llistaObservableProductos = FXCollections.observableArrayList();
 
     @FXML
     private void onClickEdit(ActionEvent event) {
@@ -62,36 +61,90 @@ public class ProductosController implements Initializable {
 
     @FXML
     private void onClickAdd(ActionEvent event) {
+        insertarProducto();
     }
 
     @FXML
-    private void onClickDelete(ActionEvent event) {
-        /*try {
-              c1.(idProductoSeleccionado);
-              cl.borrarComandaEnTableView(idProductoSeleccionado);
-        }catch(SQLException ex){
-            mostrarAlertaError("Error carregant dades: " + ex.toString());
-        }*/
+    private void onClickDelete(ActionEvent event) throws SQLException {
+        eliminarProducto();
     }
 
+    /**
+     * Initializes the controller class.
+     *
+     * @param url
+     * @param rb
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
             ArrayList<Producto> productos = aplicacio.LogicaProducto.getProductos();
-                        
+
             for (Producto s : productos) {
-                llistaObservableProductos.add(new Producto(s.getCode(), s.getNombre(),s.getDescripcion(),s.getCantidadStock(),s.getPrecio()));
+                llistaObservableProductos.add(new Producto(s.getCode(), s.getNombre(), s.getDescripcion(), s.getCantidadStock(), s.getPrecio()));
             }
             idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
-            nameCol.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+            nombreCol.setCellValueFactory(new PropertyValueFactory<>("nombre"));
             descCol.setCellValueFactory(new PropertyValueFactory<>("descripcion"));
             stockCol.setCellValueFactory(new PropertyValueFactory<>("cantidad"));
             precioCol.setCellValueFactory(new PropertyValueFactory<>("precio"));
             tablaProductos.setItems(llistaObservableProductos);
-            dades.DataSource.getConnection("m03uf6_22_23","root","123456");
-        } catch(SQLException ex) {
-            
+            dades.DataSource.getConnection("m03uf6_22_23", "root", "123456");
+        } catch (SQLException ex) {
+
         }
-    }    
+    }
+
+    // Función de selección de elemento en la tabla
+    @FXML
+    void onItem_Selected(MouseEvent event) {
+        Producto producto = (Producto) tablaProductos.getSelectionModel().getSelectedItem();
+        
+        if(producto != null) {
+            txtNombre.setText(producto.getNombre());
+            txtDesc.setText(producto.getDescripcion());
+            txtStock.setText(producto.getCantidadStock() + "");
+            txtPrecio.setText(producto.getPrecio() + "");
+        }
+    }
     
+    // Función para insertar un producto
+    public void insertarProducto() {
+        Producto producto = new Producto();
+        producto.setNombre(txtNombre.getText());
+        producto.setDescripcion(txtDesc.getText());
+        byte cantidadStock = Byte.parseByte(txtStock.getText());
+        producto.setCantidadStock(cantidadStock);
+        double precio = Double.parseDouble(txtPrecio.getText());
+        producto.setPrecio(precio);
+
+        try {
+            dades.DataSource.getConnection("m03uf6_22_23", "root", "123456");
+            aplicacio.LogicaProducto.setProducto(producto);
+            llistaObservableProductos.add(producto);
+            tablaProductos.refresh();
+            limpiarCampos();
+            
+        } catch (SQLException ex) {
+
+        }
+
+    }
+    
+    // Función para eliminar un producto de la BBDD
+    public void eliminarProducto() throws SQLException {
+        Producto producto = (Producto) tablaProductos.getSelectionModel().getSelectedItem();
+        LogicaProducto.deleteProducto(producto);
+        llistaObservableProductos.remove(producto);
+        tablaProductos.refresh();
+        limpiarCampos();
+    }
+    
+    // Función para dejar en blanco los TextField
+    public void limpiarCampos() {
+        txtNombre.clear();
+        txtDesc.clear();
+        txtPrecio.clear();
+    }
+
 }
